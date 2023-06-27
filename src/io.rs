a87a74e91fe05bcff7b62977cbb771bf955e2ff4
RickarySanchez <rickarysanchez@gmail.com>
2023-06-27 03:26:06 +0100

    add scripts/ datasets/ and an io module for reading and writing data

---
+++ src/io.rs
@@ -0,0 +1,15 @@
+fn read_csv() {
+    std::unimplemented!();
+}
+
+fn read_json() {
+    std::unimplemented!();
+}
+
+fn write_csv() {
+    std::unimplemented!();
+}
+
+fn write_json() {
+    std::unimplemented!();
+}
