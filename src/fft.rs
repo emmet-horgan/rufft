7cec92c51423db8380b06d3f4ab0d810a7f2edb7
RickarySanchez <rickarysanchez@gmail.com>
2024-06-14 23:25:42 +0100

    update to sync@

--- src/fft.rs
+++ src/fft.rs
@@ -1,13 +1,98 @@
 #![allow(non_snake_case)]
-use ndarray::{prelude::*};
-use num_complex::{Complex};
+use ndarray::prelude::*;
+use num::Zero;
+use num_complex::Complex;
 use crate::traits::{FloatVal, SigVal};
-use num_traits::float;
-use num_traits::cast::{AsPrimitive, FromPrimitive};
+use num_traits::Num;
+use num_traits::cast::AsPrimitive;
+use std::ops::DivAssign;
+use std::iter::{ExactSizeIterator, Iterator};
 
-pub fn dft<U, T: FloatVal>(x: &Array1<U>) -> Array1<Complex<T>>
+struct DftIter<I, T>
+where
+    I: ExactSizeIterator,
+    I::Item: SigVal<T>,
+    T: FloatVal
+{
+    iter: I,
+    cur: usize,
+    length: usize,
+    _marker: std::marker::PhantomData<T>
+}
+
+impl<I, T> DftIter<I, T> 
+where 
+    I: ExactSizeIterator,
+    I::Item: SigVal<T>,
+    T: FloatVal
+{
+    fn new(iter: I) -> Self {
+        let length = iter.len();
+        DftIter {
+            iter,
+            cur: 0,
+            length,
+            _marker: std::marker::PhantomData
+        }
+    }
+}
+
+impl<I, T> Iterator for DftIter<I, T>
+where
+    I: ExactSizeIterator,
+    I::Item: SigVal<T>,
+    T: FloatVal,
+    usize: AsPrimitive<T>
+{
+    type Item = Complex<T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.cur == self.length - 1 {
+            return None;
+        }
+
+        let mut sum = Complex::<T>::zero();
+        let N = self.length;
+        let N_t: T = N.as_();
+        let twopi: T = (2).as_() * T::PI();
+        let zero = T::zero();
+        let k = self.cur;
+        let mut n: usize = 0;
+        for x in self.iter.by_ref() {
+            let phase: Complex<T> = Complex::<T>::new(zero, -(twopi * n.as_() * k.as_()) / N_t);
+            sum += Complex::<T>::new(x.as_(), zero) * phase.exp();
+            n += 1
+        }
+        self.cur += 1;
+        Some(sum)
+    }
+}
+
+impl<I, T> ExactSizeIterator for DftIter<I, T>
+where
+    I: ExactSizeIterator,
+    I::Item: SigVal<T>,
+    T: FloatVal,
+    usize: AsPrimitive<T>
+{
+    fn len(&self) -> usize {
+        self.length
+    }
+}
+
+pub fn dft_func<T: FloatVal, I: ExactSizeIterator>(x: I) -> DftIter<I, T>
+where
+    I: ExactSizeIterator,
+    I::Item: SigVal<T>,
+    usize: AsPrimitive<T>
+{
+    DftIter::new(x)
+}
+
+pub fn dft<U, T>(x: &Array1<U>) -> Array1<Complex<T>>
 where
     U: SigVal<T>,
+    T: FloatVal,
     usize: AsPrimitive<T>
 {
 
@@ -31,19 +116,21 @@ where
         out[k] = sum;
     }
     
-    return out;
+    out
 }
 
-pub fn fftfreq<T>(n: usize, p: T) -> Array1<T> 
+pub fn fftfreq<U, T>(n: usize, p: U) -> Array1<T> 
 where 
-    T: float::Float + FromPrimitive + ndarray::ScalarOperand + std::ops::DivAssign,
+    U: SigVal<T>,
+    T: FloatVal,
+    usize: AsPrimitive<T>,
+    Array1<T>: DivAssign<T>
 {
-    let zero = T::from_f32(0.0).unwrap();
-    let one = T::from_f32(1.0).unwrap();
-    let two = T::from_f32(2.0).unwrap();
-    let n_t = T::from_usize(n).unwrap();
+    let zero = T::zero();
+    let one = T::one();
+    let two = T::one() + T::one();
+    let n_t: T = n.as_();
     
-
     let part0 :Array1<T>;
     let part1 :Array1<T>;
     if n % 2 == 0 {
@@ -57,25 +144,28 @@ where
     }
 
     let mut arr = ndarray::concatenate![Axis(0), part0, part1];
-    arr /= p * n_t;
+    
+    arr /= p.as_() * n_t;
     return arr;
 }
 
-pub fn dtft<'a, T>(x: &'a Array1<T>) -> impl Fn(&Array1<T>) -> Array1<Complex<T>> + 'a
+pub fn dtft<U, T>(x: Array1<U>) -> impl Fn(Array1<T>) -> Array1<Complex<T>>
 where 
-    T: float::Float + FromPrimitive + std::ops::AddAssign + std::ops::SubAssign + std::ops::MulAssign + std::ops::DivAssign + std::ops::RemAssign,
+    U: SigVal<T>,
+    T: FloatVal,
+    usize: AsPrimitive<T>,
 {
-    |samples: &Array1<T>| -> Array1<Complex<T>> {
-        let zero = T::from_f32(0.0).unwrap();
+    move |samples: Array1<T>| -> Array1<Complex<T>> {
+        let zero = T::zero();
         let mut y: Array1<Complex<T>> = Array1::<Complex<T>>::zeros(samples.len());
 
         for (index, &w) in samples.iter().enumerate() {
             let mut sum: Complex<T> = Complex::<T>::new(zero, zero);
             for n in 0..x.len() {
-                let n_t = T::from_usize(n).unwrap();
+                let n_t: T = n.as_();
                 let phase: Complex<T> = Complex::<T>::new(zero, -n_t * w);
                 
-                let arg: Complex<T> = Complex::<T>::new(x[n], zero) * phase.exp();
+                let arg: Complex<T> = Complex::<T>::new(x[n].as_(), zero) * phase.exp();
                 
                 sum += arg;
             }
@@ -85,26 +175,28 @@ where
     }
 }
 
-pub fn fft_ct<T>(x: &Array1<T>) -> Array1<Complex<T>> 
+pub fn fft_ct<U, T>(x: &Array1<U>) -> Array1<Complex<T>> 
 where 
-    T: float::Float + FromPrimitive + std::ops::AddAssign + std::ops::SubAssign + std::ops::MulAssign + std::ops::DivAssign + std::ops::RemAssign,
+    U: SigVal<T>,
+    T: FloatVal,
+    usize: AsPrimitive<T>,
 {
     let N = x.len();
-    let N_t = T::from_usize(N).unwrap();
-    let zero = T::from_f64(0.0).unwrap();
-    let one = T::from_f64(1.0).unwrap();
-    let twopi = T::from_f64(std::f64::consts::PI * 2.0).unwrap();
+    let N_t: T = N.as_();
+    let zero = T::zero();
+    let one = T::one();
+    let twopi: T = (2).as_() * T::PI();
 
     if N == 1 {
-        return array![Complex::<T>::new(x[0], zero)];
+        array![Complex::<T>::new(x[0].as_(), zero)]
     }
     else {
         let wn = Complex::<T>::new(zero, -twopi / N_t);
         let wn = wn.exp();
         let mut w = Complex::<T>::new(one, zero);
 
-        let x_even: Array1<T> = x.slice(s![0..;2]).to_owned();
-        let x_odd: Array1<T> = x.slice(s![1..;2]).to_owned();
+        let x_even: Array1<U> = x.slice(s![0..;2]).to_owned();
+        let x_odd: Array1<U> = x.slice(s![1..;2]).to_owned();
         
         let y_even = fft_ct(&x_even);
         let y_odd = fft_ct(&x_odd);
@@ -116,29 +208,42 @@ where
             y[j + N/2] = y_even[j] - tmp; 
             w *= wn;
         }
-        return y;
+        y
     }
 }
 
-pub fn fft_pf<T>(x: &Array1<T>) -> Array1<Complex<T>> 
+pub fn fft_pf<U, T>(x: &Array1<U>) -> Array1<Complex<T>> 
 where 
-    T: float::Float + FromPrimitive + std::ops::AddAssign + std::ops::SubAssign + std::ops::MulAssign + std::ops::DivAssign + std::ops::RemAssign,
+    U: SigVal<T>,
+    T: FloatVal,
+    usize: AsPrimitive<T>,
 {
     std::unimplemented!();
 }
 
-pub fn ifft_ct<T>(x: &Array1<Complex<T>>) -> Array1<T> 
+pub fn czt<U, T>(x: &Array1<U>) -> Array1<Complex<T>> 
 where 
-    T: float::Float + FromPrimitive + std::ops::AddAssign + std::ops::SubAssign + std::ops::MulAssign + std::ops::DivAssign + std::ops::RemAssign,
+    U: SigVal<T>,
+    T: FloatVal,
+    usize: AsPrimitive<T>,
 {
     std::unimplemented!();
 }
 
-pub fn zero_pad<T>(x: &Array1<T>, ) -> Option<Array1<T>> 
-where 
-    T: float::Float + FromPrimitive
+pub fn ifft_ct<T: FloatVal>(x: &Array1<Complex<T>>) -> Array1<T> 
 {
-    let zero = T::from_f64(0.0).unwrap();
+    std::unimplemented!();
+}
+
+//pub fn pad<T: Num + Copy>(x: &Array1<T>, val: T, len: usize) -> Result<Array1<T>, ndarray::ShapeError> {
+//    //let _: Array1<T> = x.iter().map(|x| x).into()
+//    //let tmp = x.clone();
+//    x.clone().into_shape((1, len))
+//}
+
+pub fn zero_pad<T: Num + Copy>(x: &Array1<T>) -> Option<Array1<T>> 
+{
+    let zero = T::zero();
     let N = x.len();
 
     // Add a check for N = 2^{m}, zero pad if not. Only executed once regardsless of recursion.
@@ -269,6 +374,61 @@ mod tests {
             }
             _ => {panic!()}
         }
+    }
+
+    #[test]
+    fn dft_sine_func() {
+        let json_data = io::read_json("datasets/fft/fft/fft.json");
+        let mut output: Array1<Complex<f64>>;
+        let epsilon = 1E-10;
+        let mut max_mag_error: f64 = 0.0;
+        let mut max_phase_error: f64 = 0.0;
+        match json_data.input_data {
+            io::Data::<f64>::Array(input) => {
+                let input: Array1<f64> = Array1::from_vec(input);
+                output = Array1::zeros()
+                let result = fft::dft_func(input.into_iter());
+                for x in result {
+                    println!("{}", x);
+                }
+            }
+            _ => {panic!()}
+        }
+        //match json_data.output_data {
+        //    io::Data::ComplexVals { mag, phase} => {
+        //        for i in 0..mag.len() {
+        //            let mag_calc = output[i].norm();
+        //            let phase_calc = output[i].arg();
+        //            let percentage_mag: f64;
+        //            let percentage_phase: f64;
+        //            if (mag[i] == f64::from(0.0)) || (mag_calc == f64::from(0.0)) {
+        //                percentage_mag = (mag[i] - mag_calc).abs();
+        //            } 
+        //            else {
+        //                percentage_mag = (mag[i] - mag_calc).abs() / mag[i];
+        //            }
+        //            if (phase[i] == f64::from(0.0)) || (phase_calc == f64::from(0.0)) {
+        //                percentage_phase = (phase[i] - phase_calc).abs();
+        //            } 
+        //            else {
+        //                percentage_phase = (phase[i] - phase_calc).abs() / phase[i];
+        //            }
+        //            if percentage_mag > max_mag_error {
+        //                max_mag_error = percentage_mag;
+        //            }
+        //            if percentage_phase > max_phase_error {
+        //                max_phase_error = percentage_phase;
+        //            }
+        //            if !((percentage_mag < epsilon) && (percentage_phase < epsilon)) {
+        //            assert!(false)
+        //            }
+        //            assert!((percentage_mag < epsilon) && (percentage_phase < epsilon));
+        //        }
+        //        println!("Maximum % magnitude error: {}", max_mag_error);
+        //        println!("Maximum % phase error: {}", max_phase_error);
+        //    }
+        //   _ => {panic!()}
+        //}
     }
 
     #[test]
--- src/traits.rs
+++ src/traits.rs
@@ -1,8 +1,8 @@
-use std::convert::Into;
-use std::ops::{Add, Div, Mul, Sub};
-use std::usize;
 use num_traits::{AsPrimitive, Num, NumAssignOps};
 use num_traits::float::{Float, FloatConst};
+use num::Complex;
+use std::iter::Iterator;
+use std::iter::ExactSizeIterator;
 
 pub trait Stats {
     type Elements;
@@ -15,16 +15,52 @@ pub trait Stats {
     fn histogram(&self);
 } 
 
-mod marker {
-    trait AsPrimitiveTo {}
-}
-
 pub trait FloatVal: Num + NumAssignOps + Float + FloatConst + 'static {}
 
 pub trait SigVal<T: FloatVal>: Num + NumAssignOps + 'static 
 where 
     Self: AsPrimitive<T>
 {}
+
+//pub trait Signal<T: FloatVal, I: ExactSizeIterator>: ExactSizeIterator + SigVal<T>
+//where 
+//    I::Item: SigVal<T>
+//{}
+
+//pub trait Signal<T: FloatVal, I: Iterator>: SigVal<T>
+//where
+//    I::Item: SigVal<T>
+//{}
+
+//pub struct SignalIter<T: FloatVal, I> {
+//    marker: std::marker::PhantomData<T>,
+//    iter: I
+//}
+
+//impl<T, I> Iterator for SignalIter<T, I> 
+//where
+//    T: FloatVal,
+//    I: Iterator,
+//    I::Item: SigVal<T>,
+//{
+//    type Item = I::Item;
+//
+//    fn next(&mut self) -> Option<Self::Item> {
+//        self.iter.next()
+//    }
+//}
+//
+//impl<T, I> Signal for SignalIter<T, I>
+//where
+//    T: FloatVal,
+//    I: Iterator,
+//    I::Item: SigVal<T>,
+//{}
+//pub trait SignalIter<T: FloatVal, I>
+//where 
+//    I: Iterator,
+//    I::Item: SigVal<T>,
+//{}
 
 impl FloatVal for f64 {}
 impl FloatVal for f32 {}
